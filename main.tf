725b261165ae6e446ce9b1caa514f596561c1da7
Andrew <ya.andrew.lo@outlook.com>
2023-05-13 23:52:30 +0800

    set up OIDC config

--- main.tf
+++ main.tf
@@ -27,3 +27,13 @@ resource "aws_eks_cluster" "eks" {
     aws_iam_role_policy_attachment.eks_AmazonEKSVPCResourceController
   ]
 }
+
+data "tls_certificate" "cert" {
+  url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "oidc" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.cert.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+}
